806137d69f5e4db0f615ab660e410aa313b89dde
jun yin <jyin25@gmail.com>
2019-06-10 15:07:34 -0700

    testing

--- src/App.js
+++ src/App.js
@@ -1,35 +1,17 @@
-import React from 'react';
-import Split from './composition/Split';
-import './App.css'
-import Tooltip from './composition/Tooltip';
+import React, { Component } from 'react';
+import './App.css';
+import Messages from './Messages';
 
-const firstTooltip = (
-  <Tooltip message='tooltip message' color='hotpink'>
-   ipsum
-  </Tooltip>
-)
-
-const secondTooltip = (
-  <Tooltip message='another tooltip message' color='#126BCC'>
-    officiis
-  </Tooltip>
-)
-
-function App() {
-  return (
-    <main className='App'>
-      <Split className='left' flexBasis='2'>
-        This is the content for the left Split. Lorem {firstTooltip} dolor sit amet consectetur, adipisicing elit. Incidunt ex velit suscipit facere officia?<br />
-        {/* make another tooltip directly inside the App */}
-        <Tooltip message='one more tooltip message'>
-          Necessitatibus?
-        </Tooltip>
-      </Split>
-      <Split className='right'>
-        This is the content for the right Split. Inventore aliquid cupiditate suscipit repellat. Quaerat quis {secondTooltip} quam fuga. Aliquid quo possimus id soluta aspernatur.
-      </Split>
-    </main>
-  )
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>YOUR APPLICATION NAME!</h1>
+        <Messages name="Messages" unread={0}/>
+        <Messages name="Notifications" unread={10}/>
+      </div>
+    );
+  }
 }
 
-export default App
+export default App;
